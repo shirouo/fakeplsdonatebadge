8eb90dfea1a1f2c7d01da8ad732d4ef08389f73b
tekuuu <111714642+tekuuu@users.noreply.github.com>
2023-03-02 21:35:19 -0500

    Create badge.lua

---
+++ badge.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Badge Awarded";
+Text = "".. game:GetService("Players").LocalPlayer.Name .. " won haz3mn's" .. ' "Witness 1M+ Smite!" award!';
+Icon = "rbxassetid://206410289";
+Duration = 4.5
+})
